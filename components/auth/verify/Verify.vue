23b74cf1f303a625884b16382e02119682f90bbc
fkaaziebu <frederickaziebu1998@gmail.com>
2024-04-10 15:04:52 +0000

    feat(auth): redesign of verification page

--- components/auth/verify/Verify.vue
+++ components/auth/verify/Verify.vue
@@ -3,24 +3,35 @@ const verification_code = ref("");
 </script>
 <template>
   <div
-    class="flex flex-col gap-6 justify-center bg-white rounded-md p-5 py-10 w-[25%]"
+    class="flex flex-col gap-6 justify-center bg-white rounded-xl p-5 py-10 w-[25%]"
   >
     <div class="flex items-center justify-center">
       <h1 class="text-2xl font-bold">Verify Code</h1>
     </div>
     <div class="flex flex-col gap-10">
-      <div class="flex flex-col gap-2">
-        <input
-          v-model="verification_code"
-          type="text"
-          id="verification_code"
-          placeholder="Enter verification code here"
-          class="flex h-12 w-full rounded-md border border-gray-300 px-3 py-2 text-sm text-zinc-950 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-900 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-        />
+      <div>
+        <label
+          for="verification_code"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Verification Code</label
+        >
+        <div class="flex flex-col gap-3">
+          <input
+            type="text"
+            id="verification_code"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Example: 3403593"
+            required
+          />
+          <p class="text-sm">
+            A verification code has been sent to your email, please check your
+            inbox
+          </p>
+        </div>
       </div>
       <div class="flex">
         <button
-          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-blue-700 w-full p-3 text-blue-50 hover:bg-blue-700/90"
+          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-gray-800 w-full p-3 text-gray-50 hover:bg-gray-900/90"
         >
           Submit
         </button>
--- pages/auth/verify/index.vue
+++ pages/auth/verify/index.vue
@@ -7,7 +7,7 @@ import Verify from "~/components/auth/verify/Verify.vue";
 </script>
 
 <template>
-  <div class="flex items-center justify-center bg-blue-600 w-full h-screen">
+  <div class="flex items-center justify-center w-full h-screen">
     <Verify />
   </div>
 </template>
