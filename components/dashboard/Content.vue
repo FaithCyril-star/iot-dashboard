924d90d2b5f20d60effb229b715f132e71db90f1
Faith Cyril <cyrilfaith17@gmail.com>
2024-04-10 17:44:16 +0000

    Add temperature check box

--- components/dashboard/Content.vue
+++ components/dashboard/Content.vue
@@ -96,6 +96,18 @@ onMounted(() => {
             >Blood Pressure</label
           >
         </div>
+        <div class="flex gap-2 items-center">
+          <input
+            id="temperature"
+            type="checkbox"
+            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            for="temperature"
+            class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >Temperature</label
+          >
+        </div>
       </div>
     </div>
   </div>
