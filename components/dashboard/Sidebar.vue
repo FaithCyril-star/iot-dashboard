896946b0c40874d812932ff853a1ece12c96e71a
fkaaziebu <frederickaziebu1998@gmail.com>
2024-04-10 14:45:40 +0000

    feat(dashboard-modal): complete add and remove device modal

--- components/dashboard/Sidebar.vue
+++ components/dashboard/Sidebar.vue
@@ -23,8 +23,24 @@ onMounted(() => {
     </div>
 
     <!-- Add Device Modal -->
-    <Modal title="Add a device" id="add-device-modal">
-      <p>Hello, this is for adding of devices</p>
+    <Modal title="Add a device" id="add-device-modal"
+    btn_main="Add Device"
+        btn_minor="Cancel"
+    >
+      <div class="flex flex-col gap-3">
+        <label for="device_id" class="text-sm font-semibold">Device Id</label>
+        <input
+          type="text"
+          id="device_id"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Example: 23456876543222"
+          required
+        />
+        <p class="text-sm text-gray-400">
+          Provide a device Id for the device that you own in order to start
+          viewing its stats
+        </p>
+      </div>
     </Modal>
 
     <div class="flex flex-col mt-5 gap-4 pt-5 pr-3">
@@ -52,8 +68,26 @@ onMounted(() => {
       </div>
 
       <!-- Remove Device Modal -->
-      <Modal title="Remove a device" id="remove-device-modal">
-        <p>Hello, this is for removing of devices</p>
+      <Modal
+        title="Remove a device"
+        id="remove-device-modal"
+        btn_main="Delete"
+        btn_minor="Cancel"
+        btn_main_color="red"
+      >
+        <div class="flex flex-col gap-3">
+          <label for="device_id" class="text-sm font-semibold">Device Id</label>
+          <input
+            type="text"
+            id="device_id"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Example: 23456876543222"
+            required
+          />
+          <p class="text-sm text-red-400">
+            Please enter device id to confirm that you would want to delete it
+          </p>
+        </div>
       </Modal>
     </div>
   </div>
--- components/modal/Modal.vue
+++ components/modal/Modal.vue
@@ -8,6 +8,18 @@ defineProps({
     type: String,
     required: true,
   },
+  btn_main: {
+    type: String,
+    default: "I accept",
+  },
+  btn_main_color: {
+    type: String,
+    default: "gray",
+  },
+  btn_minor: {
+    type: String,
+    default: "decline",
+  },
 });
 </script>
 <template>
@@ -60,16 +72,21 @@ defineProps({
           <button
             :data-modal-hide="id"
             type="button"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            class="focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            :class="[
+              btn_main_color === 'gray'
+                ? 'text-white bg-gray-700 hover:bg-gray-800'
+                : 'text-white bg-red-700 hover:bg-red-800',
+            ]"
           >
-            I accept
+            {{ btn_main }}
           </button>
           <button
             :data-modal-hide="id"
             type="button"
-            class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-gray-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
           >
-            Decline
+            {{ btn_minor }}
           </button>
         </div>
       </div>
