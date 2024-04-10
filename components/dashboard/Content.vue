56dc9fdef933af153a6f817eb2f95b60e5d0bec3
fkaaziebu <frederickaziebu1998@gmail.com>
2024-04-10 15:32:33 +0000

    feat(dashboard-datepicker): add input field for providing start and end date

--- components/dashboard/Content.vue
+++ components/dashboard/Content.vue
@@ -21,62 +21,39 @@ onMounted(() => {
 <template>
   <div class="w-full h-full bg-white p-10 flex flex-col gap-10">
     <div class="w-full flex items-center gap-5 overflow-hidden">
-      <div class="flex items-center gap-5">
-        <button
-          id="dropdownFormatButton"
-          data-dropdown-toggle="dropdownFormat"
-          class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-          type="button"
-        >
-          <div class="flex gap-2 items-center">
-            <span>View Format</span>
-            <ChevronDownIcon class="h-5 w-5" />
-          </div>
-        </button>
-        <div
-          id="dropdownFormat"
-          class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-        >
-          <ul
-            class="py-2 text-sm text-gray-700 dark:text-gray-200"
-            aria-labelledby="dropdownFormatButton"
-          >
-            <li>
-              <a
-                href="#"
-                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >Graph</a
-              >
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >Table</a
-              >
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="flex items-center gap-5">
-        <button
-          id="dropdownDefaultButton"
-          data-dropdown-toggle="dropdown"
-          class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-          type="button"
-        >
-          <div class="flex gap-2 items-center">
-            <span>Select Period</span>
-            <ChevronDownIcon class="h-5 w-5" />
-          </div>
-        </button>
-        <div
-          id="dropdown"
-          class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-        >
-          <DatePicker v-model.range="range" mode="dateTime" />
-        </div>
+      <div class="flex items-center gap-5 w-full">
+        <DatePicker v-model.range="range" mode="date">
+          <template #default="{ inputValue, inputEvents }">
+            <div class="flex justify-start items-center gap-4 w-full">
+              <div class="flex items-center w-[20%]">
+                <label
+                  for="start_date"
+                  class="block mb-2 text-sm font-medium text-gray-900 w-1/3"
+                  >Start date</label
+                >
+                <input
+                  id="start_date"
+                  class="bg-gray-100 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2/3 p-2.5"
+                  :value="inputValue.start"
+                  v-on="inputEvents.start"
+                />
+              </div>
+              <div class="flex items-center w-[20%]">
+                <label
+                  for="end_date"
+                  class="block mb-2 text-sm font-medium text-gray-900 w-1/3"
+                  >End date</label
+                >
+                <input
+                  id="end_date"
+                  class="bg-gray-100 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2/3 p-2.5"
+                  :value="inputValue.end"
+                  v-on="inputEvents.end"
+                />
+              </div>
+            </div>
+          </template>
+        </DatePicker>
       </div>
     </div>
     <div class="flex flex-col gap-10 w-full h-full">
